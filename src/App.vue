fea8736e46fb8c798f1323953f67aa5fc719d560
yinMrsir <542968439@qq.com>
2019-03-31 13:24:35 +0800

    '提交'

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,77 @@
 <style lang="less">
   @import "style/icon.css";
   @import "style/colorUi.css";
-  page{
-    background: #f1f1f1;
+  .tool-detail{
+    background: #fff;
+    padding-bottom: 20rpx;
   }
-  h1{
+
+  h1 {
+    font-size: 40rpx;
     font-weight: bold;
-    font-size: 36rpx;
+    color: #333;
+    padding-top: 20rpx;
+    text-align: center;
+    line-height: 60rpx;
+  }
+
+  .m-section {
+    color: #999;
+    font-size: 30rpx;
+    margin: 20rpx;
+    line-height: 50rpx;
+    height: 50rpx;
+  }
+
+  .m-work {
+    margin: 20rpx;
+  }
+
+  .m-input {
+    border: 1px solid #DDDDDD;
+    border-radius: 3px;
+    margin: 20rpx;
+    .picker, input {
+      height: 80rpx;
+      line-height: 80rpx;
+      width: 96%;
+      padding: 0 2%;
+      background: transparent;
+      border: none;
+      outline: none;
+    }
+  }
+
+  .m-result {
+    margin: 40rpx;
+    padding: 20rpx;
+    line-height: 50rpx;
+    color: #555555;
+    border: 1px solid #DEF5F2;
+    border-radius: 6rpx;
+    word-break: break-all;
+    word-wrap: break-word;
+    white-space: normal;
+    background: #F5FEFD;
+  }
+
+  .m-result-tit {
+    text-align: center;
+    color: #43B7A7;
+    font-size: 32rpx;
+    line-height: 60rpx;
+  }
+
+  .m-work button {
+    height: 80rpx;
+    width: 100%;
+    color: #fff;
+    text-align: center;
+    border-radius: 6rpx;
+    background: #7CD3C7;
+    border: none;
+    outline: none;
+    font-size: 30rpx;
+    line-height: 80rpx;
   }
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,40 @@
 <template>
   <div>
-    <div class="align-center padding-xl">
-      宝宝免费取名正在开发中...
-    </div>
+    <section class="tool-detail dis-bot">
+      <h1>宝宝免费起名</h1>
+      <div class="m-section">宝宝性别</div>
+      <div class="select-box">
+        <div :class="currSexIndex === index ? 'item active' : 'item'" v-for="(item, index) in sexList" :key="index" @click="bindChangeSex(index)">
+          {{item}}
+        </div>
+      </div>
+      <div class="m-section">宝宝的姓氏</div>
+      <div class="m-input">
+        <input type="text" :value="jfirstName" placeholder="姓氏不超过两个字" @input="bindKeyChange">
+      </div>
+      <div class="m-section">取名要求</div>
+      <div class="select-box">
+        <div :class="currIndex === index ? 'item active' : 'item'" v-for="(item, index) in typeList" :key="index" @click="bindChangeType(index)">
+          {{item}}
+        </div>
+      </div>
+      <div class="m-section">出生日期</div>
+      <div class="m-input">
+        <PickerYearDay ref="pickerYearDay"></PickerYearDay>
+      </div>
+      <div class="m-work">
+        <button @click="bindSubmitHander">{{isHiddenResult ? '立即取名' : '换一批'}}</button>
+      </div>
+      <div class="m-result" :hidden="isHiddenResult">
+        <div class="m-result-tit">查询结果</div>
+        <div class="m-result-tit" id="JresultTit">适合网姓{{currSexIndex === 0 ? '女' : '男'}}宝宝的{{nameList.length}}个{{currIndex === 0 ? '单' : '双'}}名字</div>
+        <div class="jresult-name" style="padding:10px 0;">
+          <ul>
+            <li v-for="(item, index) in nameList" :key="index">{{item.name}}</li>
+          </ul>
+        </div>
+      </div>
+    </section>
     <navigator
       target="miniProgram"
       open-type="navigate"
@@ -16,12 +48,122 @@
 </template>
 
 <script>
-  export default {}
+  import PickerYearDay from '../../components/base/PickerYearDay'
+  export default {
+    components: {
+      PickerYearDay
+    },
+    data () {
+      return {
+        isHiddenResult: true,
+        currIndex: 0,
+        typeList: ['单名', '双名'],
+        currSexIndex: 0,
+        sexList: ['女宝宝', '男宝宝'],
+        nameList: [],
+        jfirstName: ''
+      }
+    },
+    methods: {
+      bindChangeType (index) {
+        this.currIndex = index
+        this.isHiddenResult = true
+      },
+      bindChangeSex (index) {
+        this.currSexIndex = index
+        this.isHiddenResult = true
+      },
+      bindKeyChange (e) {
+        this.jfirstName = e.mp.detail.value
+      },
+      bindSubmitHander () {
+        var self = this
+        if (this.chk()) {
+          wx.request({
+            url: `https://wx.yinchunyu.com/xcx/ymt/intitleJsonForWap`,
+            data: {
+              wordsCount: self.currIndex + 1,
+              sex: self.currSexIndex,
+              xing: self.jfirstName
+            },
+            success (res) {
+              let data = res.data.result
+              self.nameList = data.names
+              self.isHiddenResult = false
+            }
+          })
+        }
+      },
+      chk () {
+        if (this.jfirstName.length < 1) {
+          showTopTip('你什么都不输入，让我们很为难的…')
+          return false
+        } else if (!isChinese(this.jfirstName)) {
+          showTopTip('抱歉，目前只可以取中文名哦！')
+          return false
+        } else if (this.jfirstName.length > 2) {
+          showTopTip('客官，姓氏不支持那么长滴！')
+          return false
+        } else {
+          return true
+        }
+      }
+    }
+  }
+
+  function showTopTip (text) {
+    wx.showToast({
+      title: text,
+      icon: 'none',
+      duration: 2000
+    })
+  }
+
+  function isChinese (str) {
+    var badChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    badChar += 'abcdefghijklmnopqrstuvwxyz'
+    badChar += '0123456789'
+    badChar += ' ' + '　'
+    badChar += '`~!@#$%^&()-_=+]\\|:;"\\\'<,>?/'
+    if (str === '') {
+      return false
+    }
+    for (var i = 0, l = str.length; i < l; i++) {
+      var c = str.charAt(i)
+      if (badChar.indexOf(c) > -1) {
+        return false
+      }
+    }
+    return true
+  }
 </script>
 
-<style lang="less" scoped>
-  .cu-btn{
-    display: block;
-    margin: 0 30px;
+<style lang="less">
+  .select-box{
+    display: flex;
+    padding: 20rpx;
+    .item {
+      flex: 1;
+      height: 80rpx;
+      line-height: 80rpx;
+      text-align: center;
+      color: #333;
+      border: 1px #DDDDDD solid;
+      &.active{
+        border: 1px #FD8181 solid;
+      }
+    }
   }
+  .jresult-name{
+    ul{
+      display: flex;
+      flex-wrap: wrap;
+      li{
+        width: 20%;
+        border: 1px solid #DEF5F2;
+        text-align: center;
+      }
+    }
+  }
+  table th, table td{border:1px solid #DEF5F2;}
 </style>
